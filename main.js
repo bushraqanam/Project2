54ee35816686fa69e2a0bde94c3f6d75340ed92b
bushraqanam <bqanam@gmail.com>
2024-08-06 21:09:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+//concatination//
+//templet literals//
+var food = "apple";
+var veg = "potato";
+var favFood = "Biryani";
+var num = 35;
+console.log(food, num);
+var result = food + " " + veg + " " + favFood;
+console.log(result);
+//TEMPLET LITERALS//
+var A = "i like ".concat(food, ", ").concat(veg, " and ").concat(favFood, ".\n \t my fav number is ").concat(num, ".");
+console.log(A);
+//Concatination//
+var message = "Hello Eric";
+var abc = "would you like to learn";
+var msg = "some python today";
+var comeup = message + " " + abc + " " + msg;
+console.log(comeup);
+//Templet Litterals//
+var famous = "A person";
+var quote = "who never made a mistakes";
+var show = "never tried anything new in life";
+var Assign = "".concat(famous, " said, \"").concat(quote, " ").concat(show, ".\"");
+console.log(Assign);
+var ammi = "Ammi";
+var kaha = "darwaza band kardo";
+console.log("".concat(ammi, "  ne kaha, ").concat(kaha));
+var Age = 40;
+console.log(Age);
+var myHobbys = "cooking";
+console.log(myHobbys);
+var inroll = true;
+console.log(inroll);
+var myName = "Aizah zainab";
+console.log(myName);
+myName = "Airah inshirah";
+console.log(myName);
+var my_fav = "car";
+console.log(my_fav);
+my_fav = "food";
+console.log(my_fav);
+var Hello = Age + " " + myHobbys + " ";
---
+++ main.ts
@@ -0,0 +1,51 @@
+//concatination//
+//templet literals//
+let food:string = "apple"
+let veg:string = "potato"
+let favFood:string = "Biryani"
+let num : number = 35
+console.log(food, num)
+let result: string = food+" "+veg+" "+favFood
+console.log(result)
+//TEMPLET LITERALS//
+let A:string = `i like ${food}, ${veg} and ${favFood}.\n \t my fav number is ${num}.`
+
+console.log(A)
+
+
+//Concatination//
+let message:string = "Hello Eric"
+let abc:string = "would you like to learn"
+let msg:string = "some python today"
+let comeup:string = message+" "+abc+" "+msg
+console.log(comeup)
+//Templet Litterals//
+let famous:string = "A person"
+let quote:string = "who never made a mistakes"
+let show:string = "never tried anything new in life"
+let Assign:string = `${famous} said, "${quote} ${show}."`
+console.log(Assign)
+
+let ammi:string = "Ammi"
+let kaha:string = "darwaza band kardo"
+console.log(`${ammi}  ne kaha, ${kaha}`);
+
+
+
+let Age = 40
+console.log(Age)
+let myHobbys = "cooking"
+console.log(myHobbys)
+let inroll = true
+console.log(inroll)
+let myName = "Aizah zainab"
+console.log(myName)
+myName = "Airah inshirah"
+console.log(myName)
+let my_fav = "car"
+console.log(my_fav)
+my_fav = "food"
+console.log(my_fav)
+let Hello:string = Age+" "+myHobbys+" "
+
+
